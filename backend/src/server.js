487c97890d05516397e7bdcd50d65274525ae925
stevejoy-9381 <dstevenson273@gmail.com>
2026-06-22 12:00:16 +0530

    Fix CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,11 +19,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // ── Security Middleware ────────────────────────────────────
-console.log('FRONTEND_URL =', process.env.FRONTEND_URL);
-app.use(helmet());
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: true,
     credentials: true,
   })
 );
